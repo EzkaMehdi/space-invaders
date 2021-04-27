cc482950bfe240f666610a341d19594467916462
Mehdi <ezka.mehdi@gmail.com>
2021-04-27 17:23:02 +0200

    essai parti 4 non terminé

--- src/App.js
+++ src/App.js
@@ -30,16 +30,18 @@ class App extends React.Component {
       // Le spaceship est au départ sur la colonne 23, au milieu.
       spaceshipPositionColumn: 23,
 
-      noTouchYet: true,
+      
 
       beginning: true,
       lostGame: false,
     }
 
-    this.moveForwardAlien = this.moveForwardAlien.bind(this)
+    
     this.bulletShot = this.bulletShot.bind(this)
     this.keyDownHandler = this.keyDownHandler.bind(this)
     this.toBegin = this.toBegin.bind(this)
+    this.moveForwardAlien = this.moveForwardAlien.bind(this)
+    
   }
 
   componentDidUpdate() {
@@ -50,57 +52,20 @@ class App extends React.Component {
 
   // 3/ La fonction toBegin change le state beginning en false.
   toBegin() {
+    
     this.setState({
       beginning: false,
       // Au départ displayAlien est un array vide. 
       // Grâce à la méthode fill on lui ajoute 10 éléments de valeurs true à partir de l'index 0
-      
+    
     })
+    return(this.moveForwardAlien)
   }
 
   // 5/ Ici la touche 39 représente le keycod de la flêche de droite
   // touche 37 keycode de la flêche de gauche
   // touche 32 keycode de la touche espace
-  keyDownHandler(e) {
-
-    // Lorsque l'on bouge le spaceship pour la 1ère fois ou on tir alors les Aliens se mettent
-    // à avancer au même moment.
-    if (this.state.noTouchYet) {
-      if (e.keyCode === 39) {
-        this.moveForwardAlien()
-        this.setState({
-          spaceshipPositionColumn: this.state.spaceshipPositionColumn + 1,
-          noTouchYet: false
-        })
-      } else if (e.keyCode === 37) {
-        this.moveForwardAlien()
-        this.setState({
-          spaceshipPositionColumn: this.state.spaceshipPositionColumn - 1,
-          noTouchYet: false
-        })
-      } else if (e.keyCode === 32 && this.state.displayBullet === 'none') {
-        this.setState({ noTouchYet: false })
-        this.bulletShot();
-        this.moveForwardAlien()
-      } else {
-
-      }
-    } else {
-
-      // Lorsque le joueur tape la touche de gauche ou de droite, on change la colonne
-      // sur laquelle le spaceship est grâce à la state spaceshipPositionColumn qui est
-      // envoyé en props au composant Spaceship
-      if (e.keyCode === 39 && this.state.spaceshipPositionColumn < 40) {
-        this.setState({ spaceshipPositionColumn: this.state.spaceshipPositionColumn + 1 })
-      } else if (e.keyCode === 37 && this.state.spaceshipPositionColumn > 5) {
-        this.setState({ spaceshipPositionColumn: this.state.spaceshipPositionColumn - 1 })
-      }
-      //  Si le joueur tape sur la touche espace il appelle la fonction bulletShot
-      else if (e.keyCode === 32) {
-        this.bulletShot();
-      }
-    }
-  }
+ 
 
   // 6/ la fonction moveForwardAlien à été appellé lorsque le joueur a bouger son spaceship ou a tirer
   // pour la 1ère fois.
@@ -248,6 +213,7 @@ class App extends React.Component {
 
   render() {
     return <>{this.renderGame()}</>;
+       
   }
 }
 
